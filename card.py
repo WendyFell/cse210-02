c75310eb9c4d26746110ba3eb8138df296097ab6
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-05-09 06:16:20 -0600

    dealer and card class introduced
    I added a .py file for both the dealer and card classes.

---
+++ card.py
@@ -0,0 +1,27 @@
+import random
+
+class Card:
+    """A card can have values from 1 to 13.
+
+    The responsibility of Card is to keep track which card is drawn
+   
+    Attributes:
+        value (int): The number of the card.
+    """
+
+    def __init__(self):
+        """Constructs a new instance of Card.
+
+        Args:
+            self (Card): An instance of Card.
+        """
+        self.value = 0
+        
+    def draw(self):
+        """Generates a new random value.
+        
+        Args:
+            self (Card): An instance of Die.
+        """
+        self.value = random.randint(1, 13)
+        
---
+++ dealer.py
@@ -0,0 +1,5 @@
+from random import randint.
+number = randint(1,13)
+score = 0
+print (f"First card: {number}") (f“Current Number: str{number}")
+userGUess = input("Guess higher or lower(H or L): ")
